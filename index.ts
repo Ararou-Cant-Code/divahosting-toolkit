333e30d6b0bb3fd771c32d72e8bcb4bc14c59f9a
ararouv2 <139025622+ararouv2@users.noreply.github.com>
2023-08-31 10:13:00 -0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,9 @@
+export default class No {
+   constructor() {}
+
+   public run() {
+     return console.log("You really thought I'd be dumb enough to release the source code?");
+   }
+}
+
+new No().run();
